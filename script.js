8fd5f8ca23eaee4d073aa27ad461a81cb8c0df61
shaikhusama155 <shaikhusama155@gmail.com>
2023-06-08 11:07:09 +0500

    olx

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn = document.getElementById('btn');
+let sellForm = document.getElementById('sellForm');
+
+btn.addEventListener('click', function () {
+`<img s`
+});
+function add(){
+      `
+    <tr>
+    <td>${[i + 1]}</td>
+    <td>${students[i].name}</td>
+    <td>${students[i].rollNumber}</td>
+    <td>${students[i].math}</td>
+    <td>${students[i].eng}</td>
+    <td>${students[i].urd}</td>
+    <td>${students[i].sci}</td>
+    <td>${students[i].sst}</td>
+    <td>${students[i].sindhi}</td>
+    <td>${students[i].math + students[i].eng + students[i].urd + students[i].sci + students[i].sst + students[i].sindhi}</td>
+    <td>${((students[i].math + students[i].eng + students[i].urd + students[i].sci + students[i].sst + students[i].sindhi) * 100 / 600).toFixed(2)}%</td>
+    <tr>
+    `};
+    }
+}
